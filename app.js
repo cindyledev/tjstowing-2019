bd070f7c424c56e4cde616a247a66b6a109ce1cc
Cindy Le <cle15@myseneca.ca>
2020-01-02 16:22:29 -0500

    instafeed not working

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const nodemailer = require("nodemailer");
+const Instafeed = require("instafeed.js");
 
 const app = express();
 
@@ -96,17 +97,17 @@ app.post("/send", (req, res) => {
     secure: true, // true for 465, false for other ports
     auth: {
       user: "cottontilt@gmail.com",
-      pass: "vietnam8"
+      pass: "vietnam8",
+    tls: {
+      rejectUnauthorized: false
+    }
     }
   });
 */
 
   let transporter = nodemailer.createTransport({
     service: "gmail",
-    auth: auth,
-    tls: {
-      rejectUnauthorized: false
-    }
+    auth: auth
   });
 
   // send mail with defined transport object
@@ -130,5 +131,23 @@ app.post("/send", (req, res) => {
     });
   });
 });
+
+// Use the CDN or host the script yourself
+// https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js
+// https://matthewelsom.com/assets/js/libs/instafeed.min.js
+
+var userFeed = new Instafeed({
+  get: "user",
+  userId: "8987997106",
+  clientId: "924f677fa3854436947ab4372ffa688d",
+  accessToken: "8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9",
+  resolution: "standard_resolution",
+  template:
+    '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+  sortBy: "most-recent",
+  limit: 4,
+  links: false
+});
+userFeed.run();
 
 app.listen(port, () => console.log("Server started..."));
